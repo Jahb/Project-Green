ef5a64d817d28dd3cae085e54ac6b03ab7cbe654
vladflorea <vladflorea15@yahoo.ro>
2019-02-20 13:08:56 +0100

    started work on login ui

--- client/src/main/java/nl/tudelft/gogreen/client/Main.java
+++ client/src/main/java/nl/tudelft/gogreen/client/Main.java
@@ -1,7 +1,23 @@
 package nl.tudelft.gogreen.client;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-public class Main {
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage){
+        try{
+            Parent root = FXMLLoader.load(getClass().getResource("LoginGUI.fxml"));
+            Scene scene = new Scene(root, 650, 440);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        }catch(Exception exception){
+            exception.printStackTrace();
+        }
+    }
     public static void main(String[] args) {
-
+        launch(args);
     }
 }
---
+++ client/src/main/java/nl/tudelft/gogreen/client/MainController.java
@@ -0,0 +1,29 @@
+package nl.tudelft.gogreen.client;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
+
+
+public class MainController {
+    @FXML
+    private TextField passwordField;
+    @FXML
+    private TextField userField;
+    @FXML
+    private Button frgtPass;
+    @FXML
+    private Button loginButton;
+    @FXML
+    private Button registerButton;
+    @FXML
+    private Label loginLabel;
+
+    public void Login(ActionEvent event){
+        if(userField.getText().equals("username")&& passwordField.getText().equals("password")){
+            loginLabel.setText("Login Successful!");
+        }
+        else loginLabel.setText("Login Failed! Wrong username or password.");
+    }
+}
