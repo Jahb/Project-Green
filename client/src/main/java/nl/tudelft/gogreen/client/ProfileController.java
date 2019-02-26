45bab1da00aaace30d607d11f9d6e350382021f4
vladflorea <vladflorea15@yahoo.ro>
2019-02-26 19:23:50 +0100

    Made ListView with picture and text

--- client/src/main/java/nl/tudelft/gogreen/client/ProfileController.java
+++ client/src/main/java/nl/tudelft/gogreen/client/ProfileController.java
@@ -1,5 +1,6 @@
 package nl.tudelft.gogreen.client;
 
+
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -9,14 +10,20 @@ import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
+import javafx.scene.control.SelectionMode;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.ImagePattern;
 import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
+import javafx.util.Callback;
+import java.io.File;
 
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public  class ProfileController implements Initializable {
@@ -24,8 +31,7 @@ public  class ProfileController implements Initializable {
     private Button backButton;
     @FXML
     private Circle profileCircle;
-    @FXML
-    private ListView<ListItem> friendsList= new ListView<ListItem>();
+
 
 
     //scene switching via button
@@ -45,21 +51,53 @@ public  class ProfileController implements Initializable {
     @FXML
     Circle achievementCircle3;
 
-    private ObservableList<ListItem> items = FXCollections.observableArrayList();
+    ListItem a = new ListItem("friend1","/achievementImage.png");
+    ListItem b = new ListItem("friend2","/achievementImage.png");
+    ListItem c = new ListItem("friend3","/achievementImage.png");
+
+
+
+
+    private final ObservableList<ListItem> items = FXCollections.observableArrayList();
+    @FXML
+    private ListView<ListItem> friendsList= new ListView<ListItem>();
+
+
 
     //setting placeholder pictures
     @Override
-    public void initialize (URL url , ResourceBundle rb) {
+    public void initialize (URL location , ResourceBundle resources){
+
         Image profileImg = new Image("/logo.png");
         Image achievementImg = new Image("/achievementImage.png");
         profileCircle.setFill(new ImagePattern(profileImg));
         achievementCircle1.setFill(new ImagePattern(achievementImg));
         achievementCircle2.setFill(new ImagePattern(achievementImg));
         achievementCircle3.setFill(new ImagePattern(achievementImg));
+
+        items.clear();
+        items.add(new ListItem("profile1", "achievementImage.png"));
+        items.add(new ListItem("profile2", "achievementImage.png"));
+        items.add(new ListItem("profile3", "achievementImage.png"));
+        friendsList.setCellFactory(new Callback<ListView<ListItem>, ListCell<ListItem>>(){
+
+                    @Override
+                    public ListCell<ListItem> call (ListView<ListItem> arg0){
+                        ListCell<ListItem> cell = new ListCell<ListItem>(){
+                            @Override
+                            protected void updateItem(ListItem item, boolean bool){
+                                super.updateItem(item, bool);
+                                if(item!=null){
+                                    Image img = new Image(getClass().getResource("/" + item.getImageLocation()).toExternalForm());
+                                    ImageView imgview = new ImageView(img);
+                                    setGraphic(imgview);
+                                    setText(item.getName());
+                                }
+                            }
+                        };
+                return cell;
+            }
+        });
         friendsList.setItems(items);
-        for(ListItem a : items){
-            a.setImage(achievementImg);
-            a.setText("friend");
-        }
     }
 }
