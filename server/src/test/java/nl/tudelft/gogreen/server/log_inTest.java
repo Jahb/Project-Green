b8449c18932964db817921867729df558de75988
Paul Stepanov <paulstepanov@yahoo.com>
2019-03-06 20:02:24 +0100

    log_in test before

--- server/src/test/java/nl/tudelft/gogreen/server/log_inTest.java
+++ server/src/test/java/nl/tudelft/gogreen/server/log_inTest.java
@@ -1,6 +1,28 @@
 package nl.tudelft.gogreen.server;
 
 
+import org.junit.Before;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.ResourceBundle;
+
 public class log_inTest {
+    private static ResourceBundle resource = ResourceBundle.getBundle("db");
+
+    @Before
+    public void createUser(){
+        try
+        {
+            Connection conn = DriverManager.getConnection(resource.getString("Postgresql.datasource.url"), resource.getString("Postgresql.datasource.username"), resource.getString("Postgresql.datasource.password"));
+            create_user.create_user("'paul'","paul", conn);
+
+        }
+        catch(Exception exception)
+        {
+            System.out.println("Error");
+        }
+    }
+
 
 }
