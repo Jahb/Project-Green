fa13b7788cd96b6731c186eac19566849a123176
Paul Stepanov <paulstepanov@yahoo.com>
2019-03-06 15:13:47 +0100

    Boolean log_in

--- server/src/main/java/nl/tudelft/gogreen/server/log_in.java
+++ server/src/main/java/nl/tudelft/gogreen/server/log_in.java
@@ -1,4 +1,10 @@
 package nl.tudelft.gogreen.server;
+import org.springframework.security.crypto.bcrypt.BCrypt;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.ResourceBundle;
 
 
@@ -12,8 +18,43 @@ public class log_in {
 
     public static String log_in() {
 
+        try {
 
+
+            Connection conn = DriverManager.getConnection(resource.getString("Postgresql.datasource.url"), resource.getString("Postgresql.datasource.username"), resource.getString("Postgresql.datasource.password"));
+            
+        } catch (Exception exception) {
+            System.out.println("There has been an error accessing the database");
+            System.out.println(exception.getMessage());
+        }
         return null;
-}
+    }
+    public static boolean  log_in(String username, String password,  Connection conn) {
+        try {
+
+
+            PreparedStatement stmt = conn.prepareStatement(resource.getString("getAllUsers"));
+            ResultSet rs = stmt.executeQuery();
+            String dbpassword = null;
+            while (rs.next()) {
+
+                if (rs.getString(2).equals(username)) {
+                    dbpassword = rs.getString(3);
+                }
+            }
+            boolean access = BCrypt.checkpw(password, dbpassword);
+
+            if (dbpassword == null) {
+                System.out.print("You are not registered, click this button to sign in!");
+                create_user.create_user();
+                return false;
+            }
+            return access;
+        } catch (Exception exception) {
+            System.out.println("There has been an error accessing the database");
+            System.out.println(exception.getMessage());
+        }
+        return false;
+    }
 
 }
