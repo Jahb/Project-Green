e3ee1b0f65ec0d76355411060e008c1bdf60b33d
Paul Stepanov <paulstepanov@yahoo.com>
2019-03-07 01:04:24 +0100

    log_in before and test

--- server/src/test/java/nl/tudelft/gogreen/server/log_inTest.java
+++ server/src/test/java/nl/tudelft/gogreen/server/log_inTest.java
@@ -1,11 +1,15 @@
 package nl.tudelft.gogreen.server;
 
 
+import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.util.ResourceBundle;
+
+import static org.junit.Assert.assertTrue;
 
 public class log_inTest {
     private static ResourceBundle resource = ResourceBundle.getBundle("db");
@@ -24,5 +28,17 @@ public class log_inTest {
         }
     }
 
+
+   
+    @Test
+
+    public void log_inTest() {
+        try {
+            Connection conn = DriverManager.getConnection(resource.getString("Postgresql.datasource.url"), resource.getString("Postgresql.datasource.username"), resource.getString("Postgresql.datasource.password"));
+            assertTrue(log_in.log_in("paul", "paul", conn));
+        } catch (Exception exception) {
+            System.out.println("Error!");
+        }
+    }
 
 }
