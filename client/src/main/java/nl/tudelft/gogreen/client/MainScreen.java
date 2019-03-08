cbca240f1f81315c7e0be0a5f07c2f6059898907
Kamron <k.l.geijsen@student.tudelft.nl>
2019-03-08 18:53:31 +0100

    Updated MainScreen fxml
    MainScreenFXML overhaul
    Updated MainScreen class to work with the new FXML
    Added new AddActivityButton class

--- client/src/main/java/nl/tudelft/gogreen/client/MainScreen.java
+++ client/src/main/java/nl/tudelft/gogreen/client/MainScreen.java
@@ -1,79 +1,81 @@
 package nl.tudelft.gogreen.client;
 
-import javafx.fxml.FXML;
+import java.io.IOException;
+import java.net.URL;
+
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.TextArea;
-import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
-
-import java.io.IOException;
-import java.net.URL;
 
 /**
  * MainScreen object.
  *
  * @author Kamron Geijsen
- * @version 4.0
+ * @version 4.20
  */
 public class MainScreen {
 
-    private Ring ring;
-    private TextArea helpText;
+	private Ring ring;
+	private TextArea helpText;
 
+	/**
+	 * Creates a scene for MainScreen.
+	 */
+	public Scene getScene() throws IOException {
+		URL url = Main.class.getResource("/MainScreen.fxml");
+		System.out.println(url);
+		StackPane root = FXMLLoader.load(url);
+		System.out.println(root);
+		
+		BorderPane baseLayer = (BorderPane) root.getChildren().get(0);
+		AnchorPane mainRingPane = (AnchorPane) baseLayer.getCenter();
+		
+		AnchorPane overlayLayer = (AnchorPane) root.getChildren().get(1);
+		BorderPane buttonsPanel = (BorderPane) overlayLayer.getChildren().get(1);
+		helpText = (TextArea) overlayLayer.getChildren().get(0);
+		
+		addMainRing(mainRingPane);
+		addIconButtons(buttonsPanel);
 
+		ring.startAnimation();
+		helpText.setVisible(false);
+		return new Scene(root, Main.getWidth(), Main.getHeight());
+	}
 
-    /**
-     * Creates a scene for MainScreen.
-     */
-    public Scene getScene() throws IOException {
-        URL url = Main.class.getResource("/MainScreen.fxml");
-        System.out.println(url);
-        AnchorPane root = FXMLLoader.load(url);
+	private void addMainRing(AnchorPane anchorPane) {
+		ring = new Ring((int) (150 * .75), 150, Main.getHeight() / 2, 200);
+		ring.addSegment(38, Color.LIME);
+		ring.addSegment(20, Color.YELLOW);
+		ring.addSegment(15, Color.GREEN);
+		anchorPane.getChildren().add(ring.getPane());
 
-        helpText = (TextArea) root.getChildren().get(0);
-        BorderPane buttonPane = (BorderPane) root.getChildren().get(1);
+		anchorPane	.widthProperty().addListener((obs, oldVal, newVal) -> ring.setX(newVal.intValue() >> 1));
+	}
 
-        addMainRing(root);
-        addIconButtons(buttonPane);
+	private void addIconButtons(BorderPane root) {
+		IconButton leaderboardButton = new IconButton("Leaderboard", 150, 150);
+		IconButton addButton = new IconButton("Add", 600, 150);
+		IconButton helpButton = new IconButton("Help", 150, 150);
 
+		root.setLeft(leaderboardButton.getStackPane());
+		root.setCenter(addButton.getStackPane());
+		root.setRight(helpButton.getStackPane());
 
-        ring.startAnimation();
-        helpText.toFront();
-        helpText.setVisible(false);
-        return new Scene(root, Main.getWidth(), Main.getHeight());
-    }
+		helpButton.setOnClick(event -> {
+			helpText.setVisible(!helpText.isVisible());
+		});
 
-    private void addMainRing(AnchorPane root) {
-        ring = new Ring((int) (150 * .75), 150, Main.getHeight() / 2, 200);
-        ring.addSegment(38, Color.LIME);
-        ring.addSegment(20, Color.YELLOW);
-        ring.addSegment(15, Color.GREEN);
-        root.getChildren().add(ring.getPane());
+		addButton.setOnClick(event -> {
+			// TODO - this event is called when addButton is clicked
 
-        root.widthProperty().addListener((obs,oldVal, newVal) -> ring.setX(newVal.intValue() >> 1));
-    }
-
-    private void addIconButtons(BorderPane root) {
-        IconButton leaderboardButton = new IconButton("Leaderboard", 150, 150);
-        IconButton addButton = new IconButton("Add", 600, 150);
-        IconButton helpButton = new IconButton("Help", 150, 150);
-
-        root.setLeft(leaderboardButton.getStackPane());
-        root.setCenter(addButton.getStackPane());
-        root.setRight(helpButton.getStackPane());
-
-        helpButton.setOnClick(event -> helpText.setVisible(!helpText.isVisible()));
-
-        addButton.setOnClick(event -> {
-            // TODO - this event is called when addButton is clicked
-
-        });
-        leaderboardButton.setOnClick(event -> {
-            Main.openLeaderboardScreen();
-        });
-    }
+		});
+		leaderboardButton.setOnClick(event -> {
+			Main.openLeaderboardScreen();
+		});
+	}
 
 }
