d9972662067f73be367423277fd24f7ee36369b8
Paul Stepanov <paulstepanov@yahoo.com>
2019-03-09 23:39:03 +0100

    started main_event

--- server/src/main/java/nl/tudelft/gogreen/server/events_main.java
+++ server/src/main/java/nl/tudelft/gogreen/server/events_main.java
@@ -1,6 +1,7 @@
 package nl.tudelft.gogreen.server;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ResourceBundle;
@@ -8,7 +9,21 @@ import java.util.ResourceBundle;
 public class events_main {
     private static ResourceBundle resource =  ResourceBundle.getBundle("db");
     public static void main_event(String username, String eventName){
+        try {
+            Connection conn = DriverManager.getConnection(resource.getString("Postgresql.datasource.url"), resource.getString("Postgresql.datasource.username"), resource.getString("Postgresql.datasource.password"));
+            System.out.println("What do you want to do?");
+            System.out.println("\t 1 - Create event ");
+            System.out.println("\t 2 - Delete event");
+            System.out.println("\t 3 - Join event");
+            System.out.println("\t 4 - Delete event ");
+            boolean loop = false;
+            int option;
+            String name;
 
+        }
+        catch (Exception exception){
+            System.out.println("An error has occurred!");
+        }
     }
     public static void create_event(String username,int idCreator, String eventName, Connection conn){
         try{
@@ -44,7 +59,6 @@ public class events_main {
             System.out.println("An error has occurred!");
         }
     }
-
     public static void leave_event(String username, String eventName, Connection conn){
         try{
             int id = new_feature.getId(username,conn);
@@ -55,7 +69,6 @@ public class events_main {
             System.out.println("An error has occurred!");
         }
     }
-    
     public static int getMaxId(Connection conn){
         try {
             PreparedStatement getMaxId = conn.prepareStatement("select event_id from event order by event_id desc limit 1;");
