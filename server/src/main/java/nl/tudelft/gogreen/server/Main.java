5b9c1622e4275dd8913b3ba2d29c8953272b24a6
Pablo <P.RodrigoValero@student.tudelft.nl>
2019-03-11 15:36:00 +0100

    Final week Version

--- server/src/main/java/nl/tudelft/gogreen/server/Main.java
+++ server/src/main/java/nl/tudelft/gogreen/server/Main.java
@@ -2,10 +2,20 @@ package nl.tudelft.gogreen.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.ResourceBundle;
+
 @SpringBootApplication
 public class Main {
+    private static ResourceBundle resource = ResourceBundle.getBundle("db");
+
     public static void main(String[] args) throws Exception {
         SpringApplication.run(Main.class, args);
-        DBmain.initial_menu("paul","paul");
+        Connection conn = DriverManager.getConnection(
+                resource.getString("Postgresql.datasource.url"),
+                resource.getString("Postgresql.datasource.username"),
+                resource.getString("Postgresql.datasource.password"));
+        CreateUser.create_user("vlad", "vladyy");
     }
 }
