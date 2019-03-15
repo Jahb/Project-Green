a2563534c3784e64e987c80d0f9579be6759dbb1
Pablo <P.RodrigoValero@student.tudelft.nl>
2019-03-15 15:56:00 +0100

    Fixing bug

---
+++ server/src/test/java/nl/tudelft/gogreen/server/NFaddingToLogTest.java
@@ -0,0 +1,12 @@
+package nl.tudelft.gogreen.server;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class NFaddingToLogTest {
+
+    @Test
+    public void addingToLog() {
+    }
+}
