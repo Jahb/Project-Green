5c16a793553f217bf436ce803b9816d701e453c1
Pablo <P.RodrigoValero@student.tudelft.nl>
2019-03-22 12:17:31 +0100

    adding stastics part 2

--- server/src/main/java/nl/tudelft/gogreen/server/Statistics.java
+++ server/src/main/java/nl/tudelft/gogreen/server/Statistics.java
@@ -13,18 +13,36 @@ public class Statistics {
 
     public static int[] getLastWeekData(int id) throws Exception {
 
-        int[] result = new int[7];
         Connection conn = getConnection(
                 resource.getString("Postgresql.datasource.url"),
                 resource.getString("Postgresql.datasource.username"),
                 resource.getString("Postgresql.datasource.password"));
-        PreparedStatement gettinWeekData = conn.prepareStatement("select total from user_history where date = current_date -? and user_id = ?");
+
+        return getLastData(id,7,conn);
+
+
+    }
+    public static int[] getLastMonthData(int id) throws Exception {
+
+        Connection conn = getConnection(
+                resource.getString("Postgresql.datasource.url"),
+                resource.getString("Postgresql.datasource.username"),
+                resource.getString("Postgresql.datasource.password"));
+
+       return getLastData(id,30,conn);
+    }
+
+    public static int[] getLastData(int id,int j, Connection conn) throws Exception {
+
+        int[] result = new int[j];
+
+        PreparedStatement gettinWeekData = conn.prepareStatement(resource.getString("qGetData"));
         gettinWeekData.setInt(2, id);
-        for (int i = 0; i < 7; i++) {
+        for (int i = 0; i < j; i++) {
             gettinWeekData.setInt(1, i);
 
             ResultSet dayData = gettinWeekData.executeQuery();
-            int totalPointsDay = -1;
+            int totalPointsDay = 0;
             while (dayData.next()) {
                 totalPointsDay = dayData.getInt(1);
             }
@@ -33,6 +51,5 @@ public class Statistics {
 
         return result;
     }
-
 
 }
