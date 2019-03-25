64b2f3a9272b60d69ba8a353351e0ea25031eb54
Pablo <P.RodrigoValero@student.tudelft.nl>
2019-03-25 11:10:53 +0100

    Creating test for userLog (last method lacking ) 100% except default

--- server/src/test/java/nl/tudelft/gogreen/server/NFactualizingUserLog2Test.java
+++ server/src/test/java/nl/tudelft/gogreen/server/NFactualizingUserLog2Test.java
@@ -19,38 +19,20 @@ public class NFactualizingUserLog2Test {
         try(Connection conn = DriverManager.getConnection(resource.getString("Postgresql.datasource.url"),
                 resource.getString("Postgresql.datasource.username"), resource.getString("Postgresql.datasource.password"))) {
             int id = NewFeature.getId("MJ", conn);
-            NewFeature.actualizingUserLog(id, "Vegetarian Meal", 20, conn);
-            int oldTotal = getTotal(id,  conn);
-            NewFeature.actualizingUserLog(id, "Vegetarian Meal", 20, conn);
-            int newTotal = getTotal(id,  conn);
 
             NewFeature.actualizingUserLog(id, "Usage of Bike", 20, conn);
             int oldTotal2 = getTotal(id,  conn);
             NewFeature.actualizingUserLog(id, "Usage of Bike", 20, conn);
             int newTotal2 = getTotal(id,  conn);
 
-            NewFeature.actualizingUserLog(id, "Lower Temperature", 20, conn);
-            int oldTotal3 =getTotal(id,  conn);
-            NewFeature.actualizingUserLog(id, "Lower Temperature", 20, conn);
-            int newTotal3 = getTotal(id,  conn);
 
-            NewFeature.actualizingUserLog(id, "Recycling", 20, conn);
-            int oldTotal4 = getTotal(id,  conn);
-            NewFeature.actualizingUserLog(id, "Recycling", 20, conn);
-            int newTotal4 = getTotal(id,  conn);
-
-            assertNotEquals(oldTotal, newTotal);
             assertNotEquals(oldTotal2, newTotal2);
-            assertNotEquals(oldTotal3, newTotal3);
-            assertNotEquals(oldTotal4, newTotal4);
-            oldTotal += 20;
+
+
             oldTotal2 += 20;
-            oldTotal3 += 20;
-            oldTotal4 += 20;
-            assertEquals(oldTotal, newTotal);
+
             assertEquals(oldTotal2, newTotal2);
-            assertEquals(oldTotal3, newTotal3);
-            assertEquals(oldTotal4, newTotal4);
+
         }catch (Exception e){
             e.printStackTrace();
         }
