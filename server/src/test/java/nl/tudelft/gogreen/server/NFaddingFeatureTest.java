7e0ff66e04683ff58441217d40e06feab91dddeb
Paul Stepanov <n.p.stepanov@student.tudelft.nl>
2019-03-28 01:11:40 +0100

    more on the testing

--- server/src/test/java/nl/tudelft/gogreen/server/NFaddingFeatureTest.java
+++ server/src/test/java/nl/tudelft/gogreen/server/NFaddingFeatureTest.java
@@ -6,6 +6,8 @@ import org.junit.Test;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.ResourceBundle;
 
 import static org.junit.Assert.assertEquals;
@@ -18,8 +20,39 @@ public class NFaddingFeatureTest {
     public void actualizingUserPoints() {
         try (Connection conn = DriverManager.getConnection(resource.getString("Postgresql.datasource.url"),
                 resource.getString("Postgresql.datasource.username"), resource.getString("Postgresql.datasource.password"))) {
-            int id = NewFeature.getId("MJ", conn);
-            assertEquals(id,0);
+            int id = NewFeature.getId("MJ",conn);
+            PreparedStatement getAccess = conn.prepareStatement(resource.getString("qReturnAccess"));
+            ResultSet rs = getAccess.executeQuery();
+
+            int access1 = -1;
+
+            while (rs.next()) {
+                access1 = rs.getInt(1);
+            }
+
+            NewFeature.adding_feature("MJ","Vegetarian Meal");
+
+            PreparedStatement streakDateNumber = conn.prepareStatement(resource.getString("qReturnDays2"));
+            streakDateNumber.setInt(1,  id);
+            ResultSet rs2 = streakDateNumber.executeQuery();
+
+            int number = -1;
+
+            while (rs2.next()) {
+                number = rs2.getInt(1);
+            }
+            assertEquals(number,1);
+
+            PreparedStatement getAccess2 = conn.prepareStatement(resource.getString("qReturnAccess"));
+            ResultSet rs3 = getAccess2.executeQuery();
+
+            int access2 = -1;
+
+            while (rs3.next()) {
+                access2 = rs3.getInt(1);
+            }
+
+            assertEquals(access1+1,access2);
 
 
 
