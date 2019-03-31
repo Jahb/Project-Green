a5e7982fc472313c8d32a846edf4f06aa8fc60fe
Pablo <P.RodrigoValero@student.tudelft.nl>
2019-03-31 12:17:53 +0200

    UsageOfPublicTransport completed API

--- server/src/main/java/nl/tudelft/gogreen/server/CoolClimateAPI.java
+++ server/src/main/java/nl/tudelft/gogreen/server/CoolClimateAPI.java
@@ -25,7 +25,7 @@ public class CoolClimateAPI {
             float holderNum = Float.parseFloat(holder);
             float holder1Num = Float.parseFloat(holder1);
             float difference = (holderNum - holder1Num) * 1000 * 1000;
-            float result = difference/365/3;
+            float result = difference / 365 / 3;
             System.out.println(result);
             return result;
         } catch (Exception e) {
@@ -45,8 +45,8 @@ public class CoolClimateAPI {
             String holder = XML.toJSONObject(Unirest.get(url).headers(params).asString().getBody()).getJSONObject("response").get("input_takeaction_go_organic_myprodcost").toString();
 
 
-            float holderNum = Float.parseFloat(holder) *1000*1000;
-            float result = holderNum/365/3;
+            float holderNum = Float.parseFloat(holder) * 1000 * 1000;
+            float result = holderNum / 365 / 3;
             System.out.println(result);
             return result;
         } catch (Exception e) {
@@ -66,8 +66,8 @@ public class CoolClimateAPI {
             String holder = XML.toJSONObject(Unirest.get(url).headers(params).asString().getBody()).getJSONObject("response").get("result_takeaction_ride_my_bike_driveghgs").toString();
 
 
-            float holderNum = Float.parseFloat(holder) *1000*1000;
-            float result = holderNum/365; //result in grams per day
+            float holderNum = Float.parseFloat(holder) * 1000 * 1000;
+            float result = holderNum / 365; //result in grams per day
             System.out.println(result);
             return result;
         } catch (Exception e) {
@@ -76,14 +76,38 @@ public class CoolClimateAPI {
         return -1;
     }
 
-    public static Map<String, String> getParams(){
+    public static float UsageofPublicTransport() {
+
+        try {
+            Map<String, String> params = getParams();
+
+            String url = getUrl();
+
+
+            String holder = XML.toJSONObject(Unirest.get(url).headers(params).asString().getBody()).getJSONObject("response").get("result_transport_total").toString();
+            String holder1 = XML.toJSONObject(Unirest.get(url).headers(params).asString().getBody()).getJSONObject("response").get("input_takeaction_take_public_transportation_gco2bus").toString();
+
+
+            float holderNum = Float.parseFloat(holder) * 1000 * 1000 / 365 /100;
+            float holderNum1 = Float.parseFloat(holder) * 6;
+            float result = holderNum - holderNum1; //result in grams per day
+            System.out.println("the total is: " + holderNum + " and the public transport one: " + holderNum1 + " and the result is: " + result);
+            return result;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return -1;
+    }
+
+    public static Map<String, String> getParams() {
         Map<String, String> params = new HashMap<>();
         params.put("accept", "application/json");
         params.put("app_id", "93af0470");
         params.put("app_key", "be1dbf535bd450c012e78261cf93c0ad");
         return params;
     }
-    public static String getUrl(){
+
+    public static String getUrl() {
         return "https://apis.berkeley.edu/coolclimate/footprint-sandbox?input_location_mode=2&input_location=New%20York%2C%20NY%2C%20USA&input_income=1&input_size=0&input_footprint_transportation_miles1=13000&input_footprint_transportation_mpg1=6&input_footprint_transportation_fuel1=0&result_takeaction_meat_reduction=1";
     }
 
