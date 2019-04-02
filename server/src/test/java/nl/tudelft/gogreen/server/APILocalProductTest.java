a9aa5de18b6595f6b51efad5f047a7b00570baa7
Paul Stepanov <n.p.stepanov@student.tudelft.nl>
2019-04-02 19:26:31 +0200

    testing API 30% done

---
+++ server/src/test/java/nl/tudelft/gogreen/server/APILocalProductTest.java
@@ -0,0 +1,36 @@
+package nl.tudelft.gogreen.server;
+
+import org.junit.Test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ResourceBundle;
+
+import static org.junit.Assert.assertEquals;
+
+public class APILocalProductTest {
+
+    private static ResourceBundle resource = ResourceBundle.getBundle("db");
+
+    @Test
+    public void LocalProductTest() {
+        try (Connection conn = DriverManager.getConnection(resource.getString("Postgresql.datasource.url"), resource.getString("Postgresql.datasource.username"), resource.getString("Postgresql.datasource.password"))){
+
+            CoolClimateAPI.LocalProduct();
+            PreparedStatement localproduct = conn.prepareStatement(resource.getString("qLocalProduct"));
+            ResultSet rs = localproduct.executeQuery();
+            float result = 0;
+            while (rs.next()) {
+                result = rs.getFloat(1);
+            }
+            assertEquals(result,1461.1871,1461.1871);
+            System.out.println(result);
+
+        } catch (Exception exception) {
+            System.out.println("Error!");
+        }
+    }
+
+}
