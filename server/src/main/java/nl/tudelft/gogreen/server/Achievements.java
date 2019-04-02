a17b40f10c20c3ce2a0a23ed97074118c2046d96
Pablo <P.RodrigoValero@student.tudelft.nl>
2019-04-02 19:40:52 +0200

    Creating Achievements functionalities

---
+++ server/src/main/java/nl/tudelft/gogreen/server/Achievements.java
@@ -0,0 +1,43 @@
+package nl.tudelft.gogreen.server;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.util.ResourceBundle;
+
+public class Achievements {
+
+    private static ResourceBundle resource = ResourceBundle.getBundle("db");
+
+    public static void addAchievement(int id, String achievementName) {
+        try {
+            Connection conn = DriverManager.getConnection(
+                    resource.getString("Postgresql.datasource.url"),
+                    resource.getString("Postgresql.datasource.username"),
+                    resource.getString("Postgresql.datasource.password"));
+
+            PreparedStatement add = conn.prepareStatement("insert into achievements values(?,?)");
+            add.setInt(1,id);
+            add.setString(2,achievementName);
+            add.execute();
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    public static void deleteAchievement(int id, String achievementName) {
+        try {
+            Connection conn = DriverManager.getConnection(
+                    resource.getString("Postgresql.datasource.url"),
+                    resource.getString("Postgresql.datasource.username"),
+                    resource.getString("Postgresql.datasource.password"));
+
+            PreparedStatement delete = conn.prepareStatement("delete from achievements where user_id = ?");
+            delete.setInt(1,id);
+            delete.execute();
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
