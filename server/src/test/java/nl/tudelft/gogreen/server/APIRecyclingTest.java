05291dbef46bc8cc13e6e0c2246c7d06baf19fc0
Paul Stepanov <n.p.stepanov@student.tudelft.nl>
2019-04-02 20:01:37 +0200

    3 more tests

--- server/src/test/java/nl/tudelft/gogreen/server/APIRecyclingTest.java
+++ server/src/test/java/nl/tudelft/gogreen/server/APIRecyclingTest.java
@@ -2,15 +2,36 @@ package nl.tudelft.gogreen.server;
 
 import org.junit.Test;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.ResourceBundle;
+
+import static junit.framework.TestCase.assertEquals;
 
 public class APIRecyclingTest {
 
     private static ResourceBundle resource = ResourceBundle.getBundle("db");
 
     @Test
-    public void Recycling() {
+    public void UsageBikeTest() {
+        try (Connection conn = DriverManager.getConnection(resource.getString("Postgresql.datasource.url"), resource.getString("Postgresql.datasource.username"), resource.getString("Postgresql.datasource.password"))){
 
+            CoolClimateAPI.Recycling();
+            PreparedStatement recycling = conn.prepareStatement(resource.getString("qRecycling"));
+            ResultSet rs = recycling.executeQuery();
+            float result = 0;
+            while (rs.next()) {
+                result = rs.getFloat(1);
+            }
+            assertEquals(result,709.16895,709.16895);
+            System.out.println(result);
+
+
+        } catch (Exception exception) {
+            System.out.println("Error!");
+        }
     }
 
 }
