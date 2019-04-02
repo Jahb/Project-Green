c1335cfb30b5d849af754fd8f52703a132d36464
Paul Stepanov <n.p.stepanov@student.tudelft.nl>
2019-04-02 19:59:06 +0200

    2 more classes tested

--- server/src/test/java/nl/tudelft/gogreen/server/APILowerTemperatureTest.java
+++ server/src/test/java/nl/tudelft/gogreen/server/APILowerTemperatureTest.java
@@ -2,15 +2,36 @@ package nl.tudelft.gogreen.server;
 
 import org.junit.Test;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.ResourceBundle;
+
+import static junit.framework.TestCase.assertEquals;
 
 public class APILowerTemperatureTest {
 
     private static ResourceBundle resource = ResourceBundle.getBundle("db");
 
     @Test
-    public void LowerTemperature() {
+    public void UsageBikeTest() {
+        try (Connection conn = DriverManager.getConnection(resource.getString("Postgresql.datasource.url"), resource.getString("Postgresql.datasource.username"), resource.getString("Postgresql.datasource.password"))){
 
+            CoolClimateAPI.LowerTemperature();
+            PreparedStatement temperature = conn.prepareStatement(resource.getString("qLowerTemperature"));
+            ResultSet rs = temperature.executeQuery();
+            float result = 0;
+            while (rs.next()) {
+                result = rs.getFloat(1);
+            }
+            assertEquals(result,19.710258,19.710258);
+            System.out.println(result);
+
+
+        } catch (Exception exception) {
+            System.out.println("Error!");
+        }
     }
 
 }
--- server/src/test/java/nl/tudelft/gogreen/server/APISolarPanelsTest.java
+++ server/src/test/java/nl/tudelft/gogreen/server/APISolarPanelsTest.java
@@ -2,15 +2,36 @@ package nl.tudelft.gogreen.server;
 
 import org.junit.Test;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.ResourceBundle;
+
+import static junit.framework.TestCase.assertEquals;
 
 public class APISolarPanelsTest {
 
     private static ResourceBundle resource = ResourceBundle.getBundle("db");
 
     @Test
-    public void SolarPanels() {
+    public void UsageBikeTest() {
+        try (Connection conn = DriverManager.getConnection(resource.getString("Postgresql.datasource.url"), resource.getString("Postgresql.datasource.username"), resource.getString("Postgresql.datasource.password"))){
 
+            CoolClimateAPI.SolarPanels();
+            PreparedStatement solar = conn.prepareStatement(resource.getString("qSolarPanels"));
+            ResultSet rs = solar.executeQuery();
+            float result = 0;
+            while (rs.next()) {
+                result = rs.getFloat(1);
+            }
+            assertEquals(result,1425.7097,1425.7097);
+            System.out.println(result);
+
+
+        } catch (Exception exception) {
+            System.out.println("Error!");
+        }
     }
 
 }
