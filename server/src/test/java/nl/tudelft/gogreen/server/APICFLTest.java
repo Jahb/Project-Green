bca2ed98eaa410ed6b738d52e0acd3c4ae68b2ef
Paul Stepanov <n.p.stepanov@student.tudelft.nl>
2019-04-12 01:32:03 +0200

    APICFLTest

---
+++ server/src/test/java/nl/tudelft/gogreen/server/APICFLTest.java
@@ -0,0 +1,22 @@
+package nl.tudelft.gogreen.server;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+
+public class APICFLTest {
+    @Test
+    public void CFLTest() {
+        try {
+
+            float save =  CoolClimateApi.CFL("100");
+            float save2 =  35000;
+            TestCase.assertEquals(save, save2);
+            System.out.println(save);
+
+
+        } catch (Exception exception) {
+            System.out.println("Error!");
+        }
+    }
+
+}
