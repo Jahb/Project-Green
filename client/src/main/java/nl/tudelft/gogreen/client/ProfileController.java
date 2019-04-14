e7cd5cbb61262351918c927a4abecaa6a460a695
Julius de Jeu <jpdejeu@hotmail.nl>
2019-04-14 12:12:43 +0200

    dont code while tired - julius after fixing a lot of stuff

--- client/src/main/java/nl/tudelft/gogreen/client/ProfileController.java
+++ client/src/main/java/nl/tudelft/gogreen/client/ProfileController.java
@@ -122,17 +122,23 @@ public class ProfileController implements Initializable {
         List<String> names = Api.current.getAchievementNames();
 
         switch (ach.size()) {
+
             case 1:
                 addAchievements(new ListItem(new Image("/images/IconCupGold.png"), names.get(ach.get(0) - 1)),
                         null, null);
+                break;
             case 2:
                 addAchievements(new ListItem(new Image("/images/IconCupGold.png"), names.get(ach.get(0) - 1)),
                         new ListItem(new Image("/images/IconCupGold.png"), names.get(ach.get(1) - 1)),
                         null);
-            default:
+                break;
+            case 3:
                 addAchievements(new ListItem(new Image("/images/IconCupGold.png"), names.get(ach.get(0) - 1)),
                         new ListItem(new Image("/images/IconCupGold.png"), names.get(ach.get(1) - 1)),
                         new ListItem(new Image("/images/IconCupGold.png"), names.get(ach.get(2) - 1)));
+                break;
+            default:
+                break;
         }
 
 
